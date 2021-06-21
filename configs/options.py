6e4ff44f6ca08d3d7f0f4c0a3781e2998a0077da
Luca Zanella <lzanella@fbk.eu>
2021-06-21 08:39:04 +0200

    Add gpu command line argument

--- configs/options.py
+++ configs/options.py
@@ -10,6 +10,7 @@ def parse_args():
     parser.add_argument('--batch_size',type=int,default=10)
     parser.add_argument('--dropout_rate',type=float,default=0.8)
     parser.add_argument('--segment_len',type=int,default=16)
+    parser.add_argument('--gpus', type=str, default='0')
 
     # for test time augmetation
     parser.add_argument('--test_ten_crop',dest='ten_crop',action='store_true')
@@ -19,7 +20,7 @@ def parse_args():
     parser.set_defaults(vis=False)
 
     args=parser.parse_args()
-    os.environ['CUDA_VISIBLE_DEVICES']=args.gpus
+    # os.environ['CUDA_VISIBLE_DEVICES']=args.gpus
     args.gpus=[i for i in range(len(args.gpus.split(',')))]
 
     if args.vis and args.MODEL!='UCF_C3D':
